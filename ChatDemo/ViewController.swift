57519142913f0d3eb3baadd4b16e75305e216d1f
hema <hema@hemas-mac.local>
2018-02-11 15:26:16 +0200

    Initial Commit

---
+++ ChatDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatDemo
+//
+//  Created by hema on 2/11/18.
+//  Copyright © 2018 UDAAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
